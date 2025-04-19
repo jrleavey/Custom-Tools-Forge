2a0e7bf8e6f851c6631f454d1ed43790b24bbfec
Jack Leavey <81404771+jrleavey@users.noreply.github.com>
2025-04-19 16:26:46 -0400

    Init Commit

---
+++ custom-tools.js
@@ -0,0 +1,71 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.tools.animalcarekit = {
+    ability: "wis",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.chemistrykit = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.climbinggear = {
+    ability: "str",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.explosiveskit = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.farmerstools = {
+    ability: "wis",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.forensickit = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.gunsmithtools = {
+    ability: "dex",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.hackingtools = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.mechanicstools = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.radiationdetector = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.scrungekit = {
+    ability: "dex",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.signalbooster = {
+    ability: "int",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.surveyorstools = {
+    ability: "wis",
+    id: ""
+  };
+
+  CONFIG.DND5E.tools.trapperstools = {
+    ability: "wis",
+    id: ""
+  };
+});
