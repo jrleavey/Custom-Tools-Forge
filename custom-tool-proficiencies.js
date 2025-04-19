4bb1fac1edf0f32f4f9102faa84da3cb6694ab17
Jack Leavey <81404771+jrleavey@users.noreply.github.com>
2025-04-19 16:32:19 -0400

    Create custom-tool-proficiencies.js

---
+++ custom-tool-proficiencies.js
@@ -0,0 +1,21 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.toolProficiencies.otov = {
+    label: "OTOV Tools",
+    tools: [
+      "animalcarekit",
+      "chemistrykit",
+      "climbinggear",
+      "explosiveskit",
+      "farmerstools",
+      "forensickit",
+      "gunsmithtools",
+      "hackingtools",
+      "mechanicstools",
+      "radiationdetector",
+      "scrungekit",
+      "signalbooster",
+      "surveyorstools",
+      "trapperstools"
+    ]
+  };
+});
